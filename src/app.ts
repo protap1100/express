996f2cafd546ead74bdff92028a4a6d82d13d6fb
protap1100 <protapb23@gmail.com>
2026-05-27 11:41:37 +0600

    jwt added

--- src/app.ts
+++ src/app.ts
@@ -7,13 +7,14 @@ import express, {
 import { userRoute } from "./modules/user/user.route";
 import { profileRoute } from "./modules/profile/profile.route";
 import { authRoute } from "./modules/auth/auth.route";
-
+import logger from "./middleware/logger";
 const app: Application = express();
 
 // Middleware
 app.use(express.json());
 app.use(express.text());
 app.use(express.urlencoded({ extended: true }));
+app.use(logger);
 
 app.get("/user", (req: Request, res: Response) => {
   res.status(200).json({
@@ -22,10 +23,8 @@ app.get("/user", (req: Request, res: Response) => {
   });
 });
 
-app.use("/api/users",userRoute);
-app.use("/api/profile",profileRoute);
-app.use("/api/auth",authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/profile", profileRoute);
+app.use("/api/auth", authRoute);
 
-
-
-export default app
+export default app;
