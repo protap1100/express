144852ec0a4496c95ab33911379d79ae5fceb7d7
protap1100 <protapb23@gmail.com>
2026-06-03 12:33:24 +0600

    some changes

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,17 +1,20 @@
 import type { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import config from "../config";
 
 const auth = () => {
   return async (req: Request, res: Response, next: NextFunction) => {
     // console.log(req.headers)
     const token = req.headers.authorization;
+    console.log(token);
     if (!token) {
       res.status(401).json({
         success: false,
         message: "Unauthorized Access!!",
       });
-    } else {
-      next();
     }
+
+    next();
   };
 };
 
